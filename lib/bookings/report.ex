723ac3f1c5d76b3312f047f0ff7a2a28cceed708
rodrigo.vasconcelos <rodrigo.vasconcelos@siagri.com.br>
2021-05-31 07:38:28 -0300

    Criado generate_report

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -7,7 +7,16 @@ defmodule Flightex.Bookings.Report do
     File.write!(file_name, booking_list)
   end
 
-  defp build_booking_list() do
+  def generate_report(%Date{} = from_date, %Date{} = to_date) do
+    {:ok,
+     BookingAgent.get_all()
+     |> Map.values()
+     |> Enum.filter(fn elem -> Date.compare(elem.complete_date, from_date) in [:eq, :gt] end)
+     |> Enum.filter(fn elem -> Date.compare(elem.complete_date, to_date) in [:eq, :lt] end)
+     |> Enum.map(fn booking -> booking_to_string(booking) end)}
+  end
+
+  defp build_booking_list do
     BookingAgent.get_all()
     |> Map.values()
     |> Enum.map(fn booking -> booking_to_string(booking) end)
@@ -17,8 +26,7 @@ defmodule Flightex.Bookings.Report do
          complete_date: complete_date,
          local_origin: local_origin,
          local_destination: local_destination,
-         user_id: user_id,
-         id: id
+         user_id: user_id
        }) do
     "#{user_id}, #{local_origin}, #{local_destination},#{complete_date}"
   end
